00d1b8d630539b0cfb7c73d574089c0b1d836115
dvojkan <vojislav.depalov@gmail.com>
2024-10-23 17:12:11 +0200

    changed tournaments state to allow easier testing

--- database/4_create_sample_tournaments.sql
+++ database/4_create_sample_tournaments.sql
@@ -1,5 +1,5 @@
 -- Apply this script over database created in step 1
 
-insert into tournament(tournamentId, name, prizePool, startdate, enddate, state) values (1, 'Test1',1000, utc_date, utc_date, 'not_started');
+insert into tournament(tournamentId, name, prizePool, startdate, enddate, state) values (1, 'Test1',1000, utc_date, utc_date, 'finished');
 
-insert into tournament(tournamentId, name, prizePool, prizeDistributionId, startdate, enddate, state) values (2, 'Test2',1000, 2, utc_date, utc_date, 'not_started');
+insert into tournament(tournamentId, name, prizePool, prizeDistributionId, startdate, enddate, state) values (2, 'Test2',1000, 2, utc_date, utc_date, 'finished');
